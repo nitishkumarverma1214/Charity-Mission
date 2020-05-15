0dff28990a36d6ee98a0653c96d74870c020a2b6
nitishkumarverma1214 <51161410+nitishkumarverma1214@users.noreply.github.com>
2020-05-15 20:10:38 +0530

    Update mission.sql

--- mission.sql
+++ mission.sql
@@ -21,6 +21,39 @@ INSERT INTO admnarea VALUES('a005','dinesh');
 
 INSERT INTO office_field_cd values ('c91','Bangalore OFFICE','BANGALORE ADDRESS','bangalore','202020','a005','c009',null,null,null,null,null,null);
 
+CREATE TABLE profession_master(Profession_cd varchar(3) not null, description varchar(50) not null, PRIMARY KEY(Profession_cd));
+
+INSERT INTO profession_master values('014','ADVOCATE'),('024','AGRICULTURE'),('038','ARMY'),('007','BISHOP');
+
+CREATE TABLE qualification_master (qual_cd varchar(3) not null, description varchar(50) not null, PRIMARY KEY(qual_cd));
+
+INSERT INTO qualification_master values('007','+1'),('008','+2'),('001','3RD'),('002','5TH'),('003','7TH'),('004','8TH');
+
+
+CREATE TABLE responsibility_master (resp_cd varchar(3) not null, description varchar(50) not null, PRIMARY KEY(resp_cd));
+
+INSERT INTO responsibility_master values('004','ASSOCIATE GENERAL SECRETARY'),('005','BOARD MEMBER'),('007','DIRECTOR OF PROMOTIONAL MINISTRY'),('006','GENERAL SECRETARY'),('009','MISSIONARY');
+
+
+CREATE TABLE relationship_master (rel_cd varchar(3) not null, description varchar(50) not null, PRIMARY KEY(rel_cd));
+
+INSERT INTO relationship_master values('003','BROTHER'),('010','BROTHER IN LAW'),('009','DAUGHTER'),('001','FATHER'),('014','GRAND DAUGHTER');
+
+
+CREATE TABLE blood_grp_master (blood_grp_cd varchar(3) not null, description varchar(50) not null, PRIMARY KEY(blood_grp_cd));
+
+INSERT INTO blood_grp_master values ('002','A NEGATIVE'),('001','A POSITIVE'),('006','AB NEGATIVE'),('005','AB POSITIVE'),('004','B NEGATIVE'),('003','B POSITIVE');
+
+CREATE TABLE language_master (lang_cd varchar(3) not null, description varchar(50) not null, PRIMARY KEY(lang_cd));
+
+INSERT INTO language_master values ('03','ENGLISH'),('02','HINDI'),('05','KANNADAM'),('04','MALAYALAM'),('07','ODIYA'),('01','TAMIL');
+
+CREATE TABLE spon_type_master (spon_type varchar(3) not null, description varchar(50) not null, PRIMARY KEY(spon_type));
+
+INSERT INTO spon_type_master values('001','MISSIONARY SPONSOR'),('002','MITC LIFE SPONSOR');
+
+
+
 
 
 
